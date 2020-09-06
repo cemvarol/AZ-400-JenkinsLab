ce711c58be8a8391c07136c651d570b5f800a5a7
cemvarol <cem.varol@outlook.com>
2020-09-06 12:37:36 +0100

    Create DL-DocDT.ps1

---
+++ DL-DocDT.ps1
@@ -0,0 +1,18 @@
+  
+$url = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
+$output = "$env:USERPROFILE\downloads\ChromeSetup.exe"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+& "$env:USERPROFILE\downloads\ChromeSetup.exe"
+
+Start-Sleep -s 40
+
+#& "C:\Program Files (x86)\google\chrome\Application\chrome.exe" https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe
+
+& "C:\Program Files (x86)\google\chrome\Application\chrome.exe" https://download.docker.com/win/stable/43884/Docker%20Desktop%20Installer.exe
+
+
+
+Start-Sleep -s 70
+
+& "$env:USERPROFILE\downloads\Docker Desktop Installer.exe"
